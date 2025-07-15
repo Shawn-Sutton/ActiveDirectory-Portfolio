e338ffe8f38863002ad02ecff2f03b1909a6ba7a
Shawn C Sutton <Shawncsutton@gmail.com>
2025-07-15 11:24:16 -0500

    Update Create-ADGroup.ps1

--- scripts/Create-ADGroup.ps1
+++ scripts/Create-ADGroup.ps1
@@ -1,13 +1,22 @@
+
 Param(
-    [Parameter(Mandatory=$true)][string]$GroupName,
-    [Parameter(Mandatory=$false)][ValidateSet("Global", "DomainLocal", "Universal")][string]$GroupScope = "Global"
+    [Parameter(Mandatory = $true)][string]$UserName,
+    [Parameter(Mandatory = $true)][string]$GroupName
 )
 
 Import-Module ActiveDirectory
 
-New-ADGroup `
-    -Name $GroupName `
-    -GroupScope $GroupScope `
-    -GroupCategory Security
+function Write-Log {
+    param ([string]$Message)
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    Add-Content -Path ".\toolkit.log" -Value "$timestamp : $Message"
+}
 
-Write-Host "Group $GroupName created successfully."
+try {
+    Add-ADGroupMember -Identity $GroupName -Members $UserName
+    Write-Host "User '$UserName' added to group '$GroupName' successfully." -ForegroundColor Green
+    Write-Log "User '$UserName' added to group '$GroupName'"
+} catch {
+    Write-Host "Failed to add user '$UserName' to group '$GroupName': $_" -ForegroundColor Red
+    Write-Log "Error adding user '$UserName' to group '$GroupName': $_"
+}
